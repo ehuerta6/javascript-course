93e97a82c6db4db7e2e06132e542fb9f20028723
Emiliano <ehuerta6@miners.utep.edu>
2025-04-10 19:51:20 -0600

    load products before AllHook

--- javascript-amazon-project/tests/checkout/orderSummaryTest.js
+++ javascript-amazon-project/tests/checkout/orderSummaryTest.js
@@ -1,9 +1,16 @@
-import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
-import {loadFromStorage, cart} from '../../data/cart.js';
+import { renderOrderSummary } from '../../scripts/checkout/orderSummary.js';
+import { loadFromStorage, cart } from '../../data/cart.js';
+import { loadProducts } from '../../data/products.js';
 
 describe('test suite: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  beforeAll((done) => {
+    loadProducts(() => {
+      done();
+    });
+  });
 
   beforeEach(() => {
     spyOn(localStorage, 'setItem');
@@ -14,15 +21,18 @@ describe('test suite: renderOrderSummary', () => {
     `;
 
     spyOn(localStorage, 'getItem').and.callFake(() => {
-      return JSON.stringify([{
-        productId: productId1,
-        quantity: 2,
-        deliveryOptionId: '1'
-      }, {
-        productId: productId2,
-        quantity: 1,
-        deliveryOptionId: '2'
-      }]);
+      return JSON.stringify([
+        {
+          productId: productId1,
+          quantity: 2,
+          deliveryOptionId: '1',
+        },
+        {
+          productId: productId2,
+          quantity: 1,
+          deliveryOptionId: '2',
+        },
+      ]);
     });
     loadFromStorage();
 
@@ -30,9 +40,9 @@ describe('test suite: renderOrderSummary', () => {
   });
 
   it('displays the cart', () => {
-    expect(
-      document.querySelectorAll('.js-cart-item-container').length
-    ).toEqual(2);
+    expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(
+      2
+    );
     expect(
       document.querySelector(`.js-product-quantity-${productId1}`).innerText
     ).toContain('Quantity: 2');
@@ -45,9 +55,9 @@ describe('test suite: renderOrderSummary', () => {
 
   it('removes a product', () => {
     document.querySelector(`.js-delete-link-${productId1}`).click();
-    expect(
-      document.querySelectorAll('.js-cart-item-container').length
-    ).toEqual(1);
+    expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(
+      1
+    );
     expect(
       document.querySelector(`.js-cart-item-container-${productId1}`)
     ).toEqual(null);
